8937534ee4c9a852f2f7e91ee07e8e63dc443d3b
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-12-14 12:33:46 +0200

    load CSV

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,17 +1,35 @@
-import { useEffect } from "react";
 import GameBoard from "./GameBoard/GameBoard";
 
 function Game() {
-  const load = function () {
+  const parseCSVTextToJSON = function (text) {
+    var lines = text.split("\n");
+    var result = [];
+    var headers;
+    for (var i = 0; i < lines.length; i++) {
+      headers = lines[i].split("\n");
+    }
+    var cont = 0;
+    for (i = 0; i < lines.length; i++) {
+      var obj = {};
+      var currentline = lines[i].split("\n");
+      for (var j = 0; j < headers.length; j++) {
+        obj[cont] = currentline[j];
+      }
+      cont++;
+      result.push(obj);
+    }
+    console.log("ceva json", result);
+  };
+
+  const loadCSV = function () {
     fetch("./BaseSizeAndMovement.csv")
       .then((response) => response.text())
       .then((responseText) => {
         console.log("ceva", responseText);
+        parseCSVTextToJSON(responseText);
       });
   };
-  useEffect(() => {
-    load();
-  }, []);
+  loadCSV();
   return <GameBoard />;
 }
 
