28097de7393c32cd18c0a0a453c7088b2a774c6e
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-12-19 22:59:05 +0200

    fix building coords

--- src/Game/GameBoard/Figurines/GameBuilding.js
+++ src/Game/GameBoard/Figurines/GameBuilding.js
@@ -5,12 +5,10 @@ function GameBuilding({ XSize, YSize, zoom, X, Y, isRuin }) {
         width: `${XSize * zoom}px`,
         height: `${YSize * zoom}px`,
         backgroundColor: `${isRuin ? "rgb(143, 143, 143)" : "rgb(62, 62, 62)"}`,
-
-        position: "relative",
+        position: "absolute",
         left: `${X * zoom}px`,
         top: `${Y * zoom}px`,
-        zIndex: 100,
-        transform: "translate(-50%, -50%)"
+        zIndex: 100
       }}
     />
   );
--- src/LeftOptions/BuildingsOptions/ListOfBuildings.js
+++ src/LeftOptions/BuildingsOptions/ListOfBuildings.js
@@ -1,6 +1,6 @@
 import { selectBuildings } from "../../Game/GameState/GameStateSlice";
 import { useSelector } from "react-redux";
-import { Grid2 } from "@mui/material";
+import { Checkbox, FormControlLabel, Grid2 } from "@mui/material";
 
 function ListOfBuildings() {
   const listOfBuildings = useSelector(selectBuildings);
@@ -8,21 +8,26 @@ function ListOfBuildings() {
     <div>
       <Grid2 container spacing={1}>
         <Grid2 size={2}>ID</Grid2>
-        <Grid2 size={2}>W</Grid2>
-        <Grid2 size={2}>H</Grid2>
-        <Grid2 size={2}>X</Grid2>
-        <Grid2 size={2}>Y</Grid2>
+        <Grid2 size={1.5}>W</Grid2>
+        <Grid2 size={1.5}>H</Grid2>
+        <Grid2 size={1.5}>X</Grid2>
+        <Grid2 size={1.5}>Y</Grid2>
         <Grid2 size={2}>Ruin</Grid2>
+        <Grid2 size={2}>Del</Grid2>
       </Grid2>
       {listOfBuildings.map((building) => {
         return (
           <Grid2 container spacing={1} key={building.buildingId}>
             <Grid2 size={2}>{building.buildingId}</Grid2>
-            <Grid2 size={2}>{building.XSize / 10}</Grid2>
-            <Grid2 size={2}>{building.YSize / 10}</Grid2>
-            <Grid2 size={2}>{building.X / 10}</Grid2>
-            <Grid2 size={2}>{building.Y / 10}</Grid2>
-            <Grid2 size={2}>{building.isRuin ? "True" : "False"}</Grid2>
+            <Grid2 size={1.5}>{building.XSize / 10}</Grid2>
+            <Grid2 size={1.5}>{building.YSize / 10}</Grid2>
+            <Grid2 size={1.5}>{building.X / 10}</Grid2>
+            <Grid2 size={1.5}>{building.Y / 10}</Grid2>
+            <Grid2 size={2}>
+              <FormControlLabel
+                control={<Checkbox checked={building.isRuin} disabled={true} />}
+              />
+            </Grid2>
           </Grid2>
         );
       })}
